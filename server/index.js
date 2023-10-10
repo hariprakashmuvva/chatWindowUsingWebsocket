dacb1a47201fc52b1fe98f025551f37a68739df6
hariprakashmuvva <harimuvva150478@gmail.com>
2023-10-10 14:28:50 +0530

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -1,1 +1,22 @@
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http, {
+    cors: {
+      origin: '*',
+    },
+	methods: ["GET", "POST"]
+});
+var PORT = 8080;
 
+// Start Server 🚀
+http.listen(PORT, () => {
+	console.log(`Node Server running on port : ${PORT}`);
+});
+
+// Connect to Socket 🔗
+io.on("connection", (socket) => {
+	console.log("Connection established");
+	socket.on("editor-data", (data) => {
+		socket.broadcast.emit("editor-data", data);
+	});
+});
